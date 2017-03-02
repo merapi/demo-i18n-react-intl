c31dc1f9c92a10d583bc6e3c2eaa3c0fd1805284
Marcin Pietruszka <marcin@webskill.pl>
2017-03-02 14:43:38 +0100

    more examples

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
 import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
+import { intlShape, injectIntl, defineMessages, FormattedMessage, FormattedNumber, FormattedDate, FormattedRelative } from 'react-intl';
 
 import logo from './logo.svg';
 import './App.css';
 
+const messages = defineMessages({
+  feesMessage: {
+    id: "Tooltip.fees",
+    defaultMessage: "Click here to understand how we calculate fees.",
+  },
+});
+
 class App extends Component {
+
   render() {
     const unreadCount = 1;
     const name = 'Marcin';
+    const { formatMessage } = this.props.intl;
 
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2><FormattedMessage
-                id="welcome"
-                defaultMessage={`Hello {name}, you have {unreadCount, number} {unreadCount, plural,
-                  one {message}
-                  other {messages}
-                }`}
-                values={{name: <b>{name}</b>, unreadCount}}
-              />
-          </h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p style={{ backgroundColor: 'lime', padding: 10, color: 'white', marginTop: 0 }}>Locale: {this.props.locale}</p>
+        <p>
+          <FormattedMessage
+            id="welcome"
+            defaultMessage={`Hello {name}, you have {unreadCount, number} {unreadCount, plural,
+              one {message}
+              other {messages}
+            }`}
+            values={{name: <b>{name}</b>, unreadCount}}
+        />
         </p>
+        <FormattedMessage
+          id="Tooltip.fees"
+          defaultMessage="Click here to understand how we calculate fees."
+        />
+        <p>func: {formatMessage(messages.feesMessage)}</p>
+        <p><FormattedNumber value={1000000} /></p>
+        <p><FormattedNumber value={1000000} style="currency" currency="USD" /></p>
+        <p><FormattedNumber value={1461.23} style="currency" currency="GBP" /></p>
+        <p><FormattedNumber value={1461.23} style="currency" currency="CHF" /></p>
+        <p><FormattedNumber value={1461.23} style="currency" currency="EUR" /></p>
+        <p><FormattedDate value={Date.now()} /></p>
+        <p><FormattedDate value={Date.now()} month="long" /></p>
+        <p><FormattedDate value={Date.now()} month="long" day="numeric" /></p>
+        <p><FormattedDate value={Date.now()} month="long" day="numeric" year="numeric" /></p>
+        <p>Posted: <FormattedRelative value={Date.now()} /></p>
+        <p>Posted: <FormattedRelative value={Date.now() - 3*60*60*1000} /></p>
       </div>
     );
   }
 }
 
-export default App;
+export default injectIntl(App);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { IntlProvider } from 'react-intl';
+
+import { IntlProvider, addLocaleData } from 'react-intl';
+import enLocaleData from 'react-intl/locale-data/en';
+import deLocaleData from 'react-intl/locale-data/de';
+import frLocaleData from 'react-intl/locale-data/fr';
+import plLocaleData from 'react-intl/locale-data/pl';
 
 import App from './App';
 import './index.css';
 
+addLocaleData([...enLocaleData, ...deLocaleData, ...frLocaleData, ...plLocaleData]);
 
+const language = (navigator.languages && navigator.languages[0]) ||
+                     navigator.language ||
+                     navigator.userLanguage;
 
 ReactDOM.render(
-  <IntlProvider locale="en">
-    <App />
-  </IntlProvider>,
+  <div>
+    <IntlProvider locale={language}>
+      <App locale="auto" />
+    </IntlProvider>
+    <IntlProvider locale="en">
+      <App locale="en" />
+    </IntlProvider>
+    <IntlProvider locale="de">
+      <App locale="de" />
+    </IntlProvider>
+    <IntlProvider locale="fr">
+      <App locale="fr" />
+    </IntlProvider>
+    <IntlProvider locale="pl">
+      <App locale="pl" />
+    </IntlProvider>
+  </div>,
   document.getElementById('root')
 );
