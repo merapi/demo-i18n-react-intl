e79d55a101629f9ce14a919c724b103a44ec9483
Marcin Pietruszka <marcin@webskill.pl>
2017-03-02 09:32:31 +0100

    add react-intl

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const unreadCount = 1;
+    const name = 'Marcin';
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2><FormattedMessage
+                id="welcome"
+                defaultMessage={`Hello {name}, you have {unreadCount, number} {unreadCount, plural,
+                  one {message}
+                  other {messages}
+                }`}
+                values={{name: <b>{name}</b>, unreadCount}}
+              />
+          </h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { IntlProvider } from 'react-intl';
+
 import App from './App';
 import './index.css';
 
+
+
 ReactDOM.render(
-  <App />,
+  <IntlProvider locale="en">
+    <App />
+  </IntlProvider>,
   document.getElementById('root')
 );
